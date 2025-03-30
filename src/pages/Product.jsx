d38e78dc6243b1732b0338e94ff437be79507eb4
Walker Moody <moodywalkertx@gmail.com>
2025-03-30 16:46:36 -0500

    fixxed display error

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -31,7 +31,7 @@ const Product = () => {
 
         {/* Product Images */}
         <div className ='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
-          <div className ='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w=[18.7%] w-full'>
+          <div className ='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
           {
             productData.image.map((item,index)=>(
               <img src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt =""/>
@@ -42,7 +42,7 @@ const Product = () => {
       </div>
 
     </div>
-  ) :<div className =' opacity-0'></div>
+  ) :<div className ='opacity-0'></div>
 }
 
 export default Product
