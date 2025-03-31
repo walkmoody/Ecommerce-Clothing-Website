39b1bd1cccc0e5d22721083d87b49fa0db85eca6
Walker Moody <moodywalkertx@gmail.com>
2025-03-30 21:45:26 -0500

    Add to Cart

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { products } from "../assets/assets"
 
 export const ShopContext = createContext();
@@ -9,11 +9,34 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
-   
+    const [cartItems,setCartItems] = useState({});
+
+    const addToCart = async (itemId, size)=>{
+
+        let cartData = structuredClone(cartItems);
+        if(cartData[itemId]){
+            if (cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart
     }
 
     return (
