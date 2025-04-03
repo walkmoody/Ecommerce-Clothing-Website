69593b8fbff5fb53a0362caa93571bcba88be26e
Walker Moody <moodywalkertx@gmail.com>
2025-04-02 23:08:15 -0500

    Fixed searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,9 +20,9 @@ const SearchBar = () => {
     },[location])
 
   return showSearch && visible ? (
-    <div className ='border-t border-b bg-gray-50 text-center'>
+    <div className ='border-t border-b text-center'>
         <div className ='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
-            <input value={search} onChange={(e)=>setSearch(e.target.value)} className ='onest-norm flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search'/>
+            <input value={search} onChange={(e)=>setSearch(e.target.value)} className ='onest-norm text-gray-100 flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search'/>
             <img className = 'w-4' src={assets.search} alt =""/>
         </div>  
         <img onClick={()=>setShowSearch(false)} className='inline w-3 cursor-pointer' src={assets.crossIcon} alt=""/>
