7c9682b9f51aee9ec2c5eefabbaa6d4d04ce7528
Walker Moody <moodywalkertx@gmail.com>
2025-04-13 19:54:12 -0500

    Multiple paths added

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("DB Connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/Ecommerce`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
 
 }
 
