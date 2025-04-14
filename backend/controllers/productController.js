1e7aa94e8a8dcbe130e6d017eeaa89bf7cd74103
Walker Moody <moodywalkertx@gmail.com>
2025-04-13 21:17:43 -0500

    added list remove single and add functions

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,17 +46,41 @@ const addProduct = async (req, res) =>{
 
 // function for list product
 const listProduct = async (req, res) =>{
+    try {
+        
+        const products = await productModel.find({});
+        res.json({success:true, products})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 // function for remove product
 const removeProduct = async (req, res) =>{
+    try{
 
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message:"Product Removed"})
+
+    } catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 // function for single product info
 const singleProduct = async (req, res) =>{
-
+    try{
+        const { productId } = req.body 
+        const product = await productModel.findById(productId)
+        res.json({success:true, product})
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 export{listProduct, addProduct, removeProduct, singleProduct}
