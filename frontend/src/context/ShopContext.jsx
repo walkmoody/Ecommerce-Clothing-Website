f1f1dacdf24236abeb3674799994fc19e3f59ce4
Walker Moody <moodywalkertx@gmail.com>
2025-04-14 22:35:28 -0500

    Pulls from db on frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { products } from "../assets/assets"
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 export const ShopContext = createContext();
 
@@ -9,9 +9,11 @@ const ShopContextProvider = (props) => {
     
     const currency = '$';
     const delivery_fee = 10;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
+    const [products, setProducts] = useState([]);
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size)=>{
@@ -78,12 +80,30 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
+    const getProductsData = async () => {
+        try {
+            
+            const response = await axios.get(backendUrl + '/api/product/list')
+            if(response.data.success){
+                setProducts(response.data.products)
+            }else{
+                toast.error(response.data.message)
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+    useEffect(()=> {
+        getProductsData()
+    },[])
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount,updateQuantity,
-        getCartAmount, navigate,
+        getCartAmount, navigate, backendUrl
     }
 
     return (
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -70,7 +70,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter();
-  }, [category, subCategory,search,showSearch])
+  }, [category, subCategory,search,showSearch, products])
 
   useEffect(()=>{
     sortProduct();
